7c628d7aa58e4c8903c12009e395c9c3d2972ec0
Xon <tazipper1@hotmail.com>
2018-02-22 01:47:31 +0800

    Add WarningDefault table + entity

---
+++ upload/src/addons/SV/WarningImprovements/Entity/WarningDefault.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of a XenForo add-on.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SV\WarningImprovements\Entity;
+
+use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Structure;
+
+/**
+ *
+ * @property int warning_default_id
+ * @property int threshold_points
+ * @property string expiry_type
+ * @property int expiry_extension
+ * @property bool active
+ */
+class WarningDefault extends Entity
+{
+    /**
+     * @param Structure $structure
+     * @return Structure
+     */
+    public static function getStructure(Structure $structure)
+    {
+        $structure->table = 'xf_sv_warning_default';
+        $structure->shortName = 'SV\WarningImprovements:WarningDefault';
+        $structure->primaryKey = 'warning_default_id';
+        $structure->columns = [
+            'warning_default_id' => ['type' => self::UINT, 'autoIncrement' => true, 'nullable' => true],
+            'threshold_points'   => ['type' => self::UINT, 'required' => true, 'max' => 65535],
+            'expiry_type'        => [
+                'type'          => self::STR, 'default' => 'never',
+                'allowedValues' => ['never', 'days', 'weeks', 'months', 'years']
+            ],
+            'expiry_extension'   => ['type' => self::UINT, 'default' => 0, 'max' => 65535],
+            'active'             => ['type' => self::BOOL, 'required' => true],
+        ];
+        $structure->getters = [];
+        $structure->relations = [];
+
+        return $structure;
+    }
+}
--- upload/src/addons/SV/WarningImprovements/Setup.php
+++ upload/src/addons/SV/WarningImprovements/Setup.php
@@ -6,10 +6,58 @@ use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
+use XF\Db\Schema\Alter;
+use XF\Db\Schema\Create;
 
 class Setup extends AbstractSetup
 {
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
+
+	public function installStep1()
+    {
+        $sm = $this->schemaManager();
+
+        foreach($this->getTables() as $tableName => $callback)
+        {
+            $sm->createTable($tableName, $callback);
+        }
+    }
+
+
+    public function upgrade2000000Step1()
+    {
+        $sm = $this->schemaManager();
+
+        foreach($this->getTables() as $tableName => $callback)
+        {
+            $sm->alterTable($tableName, $callback);
+        }
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function getTables()
+    {
+        $tables = [];
+
+        $tables['xf_sv_warning_default'] = function ($table) {
+            /** @var Create|Alter $table */
+            if ($table instanceof Create)
+            {
+                $table->checkExists(true);
+            }
+            $table->addColumn('warning_default_id', 'int');
+            $table->addColumn('threshold_points', 'smallint')->setDefault(0);
+            $table->addColumn('expiry_type', 'enum')->values(['never','days','weeks','months','years'])->setDefault('never');
+            $table->addColumn('expiry_extension', 'smallint')->setDefault(0);
+            $table->addColumn('active', 'tinyint', 3)->setDefault(1);
+            $table->addPrimaryKey('warning_default_id');
+        };
+
+        return $tables;
+    }
 }
