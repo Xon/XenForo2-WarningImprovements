5ee7830d76e931c16d68487ee1bce12395a4bbdb
Xon <tazipper1@hotmail.com>
2018-02-22 02:08:03 +0800

    Add WarningCategory entity & table creates

---
+++ upload/src/addons/SV/WarningImprovements/Entity/WarningCategory.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of a XenForo add-on.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SV\WarningImprovements\Entity;
+
+use SV\WarningImprovements\XF\Entity\WarningDefinition;
+use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Structure;
+
+/**
+ *
+ * @property int warning_category_id
+ * @property int threshold_points
+ * @property string expiry_type
+ * @property int expiry_extension
+ * @property bool active
+ *
+ * @property WarningCategory Parent
+ * @property WarningCategory[] ChildCategories
+ * @property WarningDefinition[] WarningDefinitions
+ */
+class WarningCategory extends Entity
+{
+    public function verifyParentWarningCategoryId()
+    {
+        throw new \LogicException('not implemented');
+    }
+
+    /**
+     * @param Structure $structure
+     * @return Structure
+     */
+    public static function getStructure(Structure $structure)
+    {
+        $structure->table = 'xf_sv_warning_default';
+        $structure->shortName = 'SV\WarningImprovements:WarningDefault';
+        $structure->primaryKey = 'warning_default_id';
+        $structure->columns = [
+            'warning_category_id'        => ['type' => self::UINT, 'autoIncrement' => true, 'nullable' => true],
+            'parent_warning_category_id' => ['type' => self::UINT, 'nullable' => true],
+            'display_order'              => ['type' => self::UINT, 'default' => 0],
+            'allowed_user_group_ids'     => [
+                'type' => self::LIST_COMMA, 'default' => [2],
+                'list' => ['type' => 'posint', 'unique' => true, 'sort' => SORT_NUMERIC]
+            ],
+
+        ];
+        $structure->getters = [];
+        $structure->relations = [
+            'Parent' => [
+                'entity' => 'SV\WarningImprovements:WarningDefault',
+                'type' => self::TO_ONE,
+                'conditions' => [['warning_category_id', '=', '$parent_warning_category_id']],
+                'primary' => true
+            ],
+            'ChildCategories' => [
+                'entity' => 'SV\WarningImprovements:WarningDefault',
+                'type' => self::TO_MANY,
+                'conditions' => [['parent_warning_category_id', '=', '$warning_category_id']],
+                'primary' => true
+            ],
+            'WarningDefinitions' => [
+                'entity' => 'XF:WarningDefinition',
+                'type' => self::TO_MANY,
+                'conditions' => [['sv_warning_category_id', '=', '$warning_category_id']],
+                'primary' => true
+            ],
+        ];
+
+        return $structure;
+    }
+}
--- upload/src/addons/SV/WarningImprovements/Setup.php
+++ upload/src/addons/SV/WarningImprovements/Setup.php
@@ -11,31 +11,46 @@ use XF\Db\Schema\Create;
 
 class Setup extends AbstractSetup
 {
-	use StepRunnerInstallTrait;
-	use StepRunnerUpgradeTrait;
-	use StepRunnerUninstallTrait;
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
 
-	public function installStep1()
+    public function installStep1()
     {
         $sm = $this->schemaManager();
-
-        foreach($this->getTables() as $tableName => $callback)
+        foreach ($this->getTables() as $tableName => $callback)
         {
             $sm->createTable($tableName, $callback);
         }
     }
 
-
     public function upgrade2000000Step1()
     {
-        $sm = $this->schemaManager();
+        $db = $this->db();
 
-        foreach($this->getTables() as $tableName => $callback)
+        $this->installStep1();
+
+        $sm = $this->schemaManager();
+        foreach ($this->getTables() as $tableName => $callback)
         {
             $sm->alterTable($tableName, $callback);
         }
+
+        $db->query('
+            UPDATE xf_sv_warning_category
+            SET parent_warning_category_id = NULL
+            WHERE parent_warning_category_id = 0'
+        );
     }
 
+    public function uninstallStep1()
+    {
+        $sm = $this->schemaManager();
+        foreach ($this->getTables() as $tableName => $callback)
+        {
+            $sm->dropTable($tableName);
+        }
+    }
 
     /**
      * @return array
@@ -52,11 +67,34 @@ class Setup extends AbstractSetup
             }
             $table->addColumn('warning_default_id', 'int');
             $table->addColumn('threshold_points', 'smallint')->setDefault(0);
-            $table->addColumn('expiry_type', 'enum')->values(['never','days','weeks','months','years'])->setDefault('never');
+            $table->addColumn('expiry_type', 'enum')->values(['never', 'days', 'weeks', 'months', 'years'])->setDefault('never');
             $table->addColumn('expiry_extension', 'smallint')->setDefault(0);
             $table->addColumn('active', 'tinyint', 3)->setDefault(1);
             $table->addPrimaryKey('warning_default_id');
         };
+
+        $tables['xf_sv_warning_category'] = function ($table) {
+            /** @var Create|Alter $table */
+            if ($table instanceof Create)
+            {
+                $table->checkExists(true);
+            }
+            $table->addColumn('warning_category_id', 'int');
+            $table->addColumn('parent_warning_category_id', 'int')->nullable(true)->setDefault(null);
+            $table->addColumn('display_order', 'int')->setDefault(0);
+            $table->addColumn('allowed_user_group_ids', 'varbinary', 255)->setDefault('2');
+            $table->addPrimaryKey('warning_category_id');
+        };
+
+        return $tables;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAlterTables()
+    {
+        $tables = [];
 
         return $tables;
     }
