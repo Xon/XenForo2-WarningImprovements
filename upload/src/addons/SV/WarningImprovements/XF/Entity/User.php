ffff14c74081102c94a31130dd59d30c7184109a
ticktackk <ims3kar@gmail.com>
2018-02-23 13:05:58 +0530

    Make required changes for template modification sv_warningimprovements_member_view1

--- upload/src/addons/SV/WarningImprovements/XF/Entity/User.php
+++ upload/src/addons/SV/WarningImprovements/XF/Entity/User.php
@@ -10,9 +10,63 @@ use XF\Mvc\Entity\Structure;
  */
 class User extends XFCP_User
 {
-    public function canViewWarnings()
+    public function canViewWarningActions(&$error = null, User $asUser = null)
     {
-        return parent::canViewWarnings();
+        $asUser = $asUser ?: \XF::visitor();
+
+        if (!$asUser->user_id)
+        {
+            return false;
+        }
+
+        if ($asUser->user_id == $this->user_id)
+        {
+            return \XF::options()->sv_view_own_warnings;
+        }
+
+        return $asUser->hasPermission('general', 'sv_viewWarningActions');
+    }
+
+    public function canViewNonSummaryWarningActions(&$error = null, User $asUser = null)
+    {
+        $asUser = $asUser ?: \XF::visitor();
+
+        if (!$asUser->user_id)
+        {
+            return false;
+        }
+
+        return $asUser->hasPermission('general', 'sv_showAllWarningActions');
+    }
+
+    public function canViewDiscouragedWarningActions(&$error = null, User $asUser = null)
+    {
+        $asUser = $asUser ?: \XF::visitor();
+
+        if (!$asUser->user_id)
+        {
+            return false;
+        }
+
+        $showDiscouragedWarningActions = \XF::options()->sv_show_discouraged_warning_actions;
+
+        switch ($showDiscouragedWarningActions)
+        {
+            case 0: // Admin/Mod/User
+                return $asUser->is_admin || $asUser->is_moderator || ($this->user_id == $asUser->user_id);
+            case 1: // Admin/Mod
+                return $asUser->is_admin || $asUser->is_moderator;
+            case 2: // Admin
+                return $asUser->is_admin;
+            case 3:
+            default: // None
+                return false;
+        }
+    }
+
+    public function getWarningActionsCount()
+    {
+        return 0; // need to work on
     }
 
     /**
@@ -22,6 +76,8 @@ class User extends XFCP_User
     public static function getStructure(Structure $structure)
     {
         $structure = parent::getStructure($structure);
+
+        $structure->getters['warning_actions_count'] = true;
     
         return $structure;
     }
