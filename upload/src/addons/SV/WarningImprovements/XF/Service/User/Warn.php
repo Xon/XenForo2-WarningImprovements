c0a08d0f09d8b7635e257dbe1d04227bad6020f4
ticktackk <ims3kar@gmail.com>
2018-02-26 07:16:17 +0530

    Additional conversation substitution replaceable for the warning conversation

--- upload/src/addons/SV/WarningImprovements/XF/Service/User/Warn.php
+++ upload/src/addons/SV/WarningImprovements/XF/Service/User/Warn.php
@@ -7,5 +7,25 @@ namespace SV\WarningImprovements\XF\Service\User;
  */
 class Warn extends XFCP_Warn
 {
+    protected function setupConversation(\XF\Entity\Warning $warning)
+    {
+        /** @var \XF\Service\Conversation\Creator $creator */
+        $creator = parent::setupConversation($warning);
 
+        $conversationTitle = $this->conversationTitle;
+        $conversationMessage = $this->conversationMessage;
+
+        $replace = [
+            '{points}' => $warning->points,
+            '{warning_title}' => $warning->title,
+            '{warning_link}' => \XF::app()->router('public')->buildLink('canonical:warnings', $warning),
+        ];
+
+        $conversationTitle = strtr(strval($conversationTitle), $replace);
+        $conversationMessage = strtr(strval($conversationMessage), $replace);
+
+        $creator->setContent($conversationTitle, $conversationMessage);
+
+        return $creator;
+    }
 }
