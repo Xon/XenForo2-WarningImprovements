a46edaf43323a150e2bb8e020dcec4a13bebb082
ticktackk <ims3kar@gmail.com>
2018-02-27 06:39:56 +0530

    Remove class extension for 'XF\Entity\ConversationMessage'

--- upload/src/addons/SV/WarningImprovements/_output/extension_hint.php
+++ upload/src/addons/SV/WarningImprovements/_output/extension_hint.php
@@ -21,7 +21,6 @@ namespace SV\WarningImprovements\XF\ControllerPlugin
 namespace SV\WarningImprovements\XF\Entity
 {
 	class XFCP_ConversationMaster extends \XF\Entity\ConversationMaster {}
-	class XFCP_ConversationMessage extends \XF\Entity\ConversationMessage {}
 	class XFCP_Phrase extends \XF\Entity\Phrase {}
 	class XFCP_User extends \XF\Entity\User {}
 	class XFCP_UserBan extends \XF\Entity\UserBan {}
