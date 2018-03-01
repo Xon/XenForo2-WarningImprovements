2c147cdc0781e1e4b05fb2c7fc1ccfd2d8e6b9e9
ticktackk <ims3kar@gmail.com>
2018-03-01 21:17:59 +0530

    Some more fixes for warning actions

--- upload/src/addons/SV/WarningImprovements/XF/Pub/Controller/Member.php
+++ upload/src/addons/SV/WarningImprovements/XF/Pub/Controller/Member.php
@@ -26,23 +26,72 @@ class Member extends XFCP_Member
             throw $this->exception($this->noPermission());
         }
 
+        if ($user->warning_actions_count === 0)
+        {
+            return $this->message(\XF::phrase('this_member_has_no_warning_actions_available'));
+        }
+
         $viewParams = [
             'user' => $user
         ];
         return $this->view('XF:Member\WarningActions\List', 'sv_member_warning_actions', $viewParams);
     }
 
-    public function actionViewWarningAction()
+    public function actionViewWarningAction(ParameterBag $params)
     {
         $userChangeTempId = $this->filter('warning_action_id', 'uint');
+
+        /** @noinspection PhpUndefinedFieldInspection */
+        /** @var \SV\WarningImprovements\XF\Entity\User $user */
+        $user = $this->assertViewableUser($params->user_id);
 
         $userChangeTemp = $this->assertWarningActionViewable($userChangeTempId);
 
         $viewParams = [
-            'warning_action' => $userChangeTemp
+            'user' => $user,
+
+            'warningAction' => $userChangeTemp
         ];
 
         return $this->view('XF:Member\WarningActions\View', 'sv_warning_actions_info', $viewParams);
+    }
+
+    public function actionWarningActionsExpire(ParameterBag $params)
+    {
+        $this->assertPostOnly();
+        $userChangeTempId = $this->filter('warning_action_id', 'uint');
+
+        /** @noinspection PhpUndefinedFieldInspection */
+        /** @var \SV\WarningImprovements\XF\Entity\User $user */
+        $user = $this->assertViewableUser($params->user_id);
+
+        $userChangeTemp = $this->assertWarningActionViewable($userChangeTempId);
+
+        if (!$userChangeTemp->canEditWarningActions($error))
+        {
+            throw $this->exception($this->noPermission($error));
+        }
+
+        if ($this->filter('expire', 'str') == 'now')
+        {
+            $expiryDate = \XF::$time;
+        }
+        else
+        {
+            $expiryLength = $this->filter('expiry_value', 'uint');
+            $expiryUnit = $this->filter('expiry_unit', 'str');
+
+            $expiryDate = strtotime("+$expiryLength $expiryUnit");
+            if ($expiryDate >= pow(2, 32) - 1)
+            {
+                $expiryDate = 0;
+            }
+        }
+
+        $userChangeTemp->expiry_date = $expiryDate;
+        $userChangeTemp->save();
+
+        return $this->redirect($this->getDynamicRedirect());
     }
 
     protected function assertViewableUser($userId, array $extraWith = [], $basicProfileOnly = false)
