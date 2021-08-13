1ffd6985fddabdfd266d20de9bd35b169f5fa535
Xon <tazipper1@hotmail.com>
2021-08-13 20:28:41 +0800

    Make deleted warning alerts how up

--- upload/src/addons/SV/WarningImprovements/Alert/WarningAlert.php
+++ upload/src/addons/SV/WarningImprovements/Alert/WarningAlert.php
@@ -6,12 +6,54 @@
 namespace SV\WarningImprovements\Alert;
 
 use XF\Alert\AbstractHandler;
+use XF\Mvc\Entity\AbstractCollection;
+use XF\Mvc\Entity\ArrayCollection;
 use XF\Mvc\Entity\Entity;
+use XF\Entity\Warning as WarningEntity;
 
 class WarningAlert extends AbstractHandler
 {
     public function canViewContent(Entity $entity, &$error = null)
     {
         return true;
+    }
+
+    protected function getPlaceholderWarning(): WarningEntity
+    {
+        /** @var WarningEntity $warning */
+        $warning = \XF::app()->em()->create('XF:Warning');
+        $warning->user_id = \XF::visitor()->user_id;
+        $warning->setReadOnly(true);
+
+        return $warning;
+    }
+
+    public function getContent($id)
+    {
+        if ($id instanceof AbstractCollection)
+        {
+            $id = $id->toArray();
+        }
+
+        if (\is_array($id))
+        {
+            $warnings = parent::getContent($id);
+
+            if (\in_array(0, $id, true))
+            {
+                $warningArr = $warnings->toArray();
+                $warningArr[0] = $this->getPlaceholderWarning();
+                $warnings = new ArrayCollection($warningArr);
+            }
+
+            return $warnings;
+        }
+
+        if ($id === 0)
+        {
+            return $this->getPlaceholderWarning();
+        }
+
+        return parent::getContent($id);
     }
 }
