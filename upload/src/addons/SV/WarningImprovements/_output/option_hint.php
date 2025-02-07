f3e27e8a49fc7b142fea588b2964e8532579e009
Xon <635541+Xon@users.noreply.github.com>
2025-02-07 13:24:54 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/WarningImprovements/_output/option_hint.php
@@ -0,0 +1,41 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property non-negative-int|null $svPreviousWarningLimit Previous warning limit
+ * @property int|null $svWarningEscalatingDefaultsLimit Escalating default warning expiry - Old-age limit
+ * @property bool|null $svWarningImprovAnonymizeConversations Anonymize warning conversations
+ * @property string|null $svWarningImprov_censorWarningTitle Censor warning titles
+ * @property bool|null $svWarningRedirect After issuing warning, redirect to content
+ * @property non-negative-int|null $svWarningsOnProfileAgeLimit Profile warning tab - Old-age limit
+ * @property bool|null $sv_force_conversation_email_on_warning Force conversation email on warning
+ * @property bool|null $sv_only_force_warning_email_on_banned Only send warning email if user is banned
+ * @property int|null $sv_post_warning_summary Post warning summary to thread
+ * @property int|null $sv_post_warning_summaryForum Post warning summary to a new thread
+ * @property bool|null $sv_respect_receive_admin_email_on_warning Respect receive site mailings setting
+ * @property non-negative-int|null $sv_show_discouraged_warning_actions Show discouraged warning action
+ * @property bool|null $sv_view_own_warnings View own warnings
+ * @property string|null $sv_warning_date_format Warning date format
+ * @property bool|null $sv_warningimprovements_alert_send_default Send warning alert by default
+ * @property bool|null $sv_warningimprovements_conversation_invite Allow invite into warning conversations by default
+ * @property bool|null $sv_warningimprovements_conversation_locked Lock warning conversations
+ * @property bool|null $sv_warningimprovements_conversation_send_default Send warning conversations by default
+ * @property bool|null $sv_warningimprovements_copy_title Copy warning title to public warning
+ * @property string|null $sv_warningimprovements_default_content_action Default content action
+ * @property bool|null $sv_warningimprovements_sticky_button Make "Warn" button sticky
+ * @property int|null $sv_warningimprovements_warning_user Anonymise as
+ * @property bool|null $sv_wi_require_warning_notes Require notes for warning
+ * @property non-negative-int|null $sv_wi_warning_note_chars Minimum note length
+ */
+class Options
+{
+}
